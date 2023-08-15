48fe87bd13d817148b498a4addbc40b5ec0ffc78
Rizwan Fahim <119812012+Gitrizwan-web@users.noreply.github.com>
2023-08-15 01:39:12 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
@@ -24,38 +25,33 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-      document.title = "TextUtils - Dark Mode";
-      // setInterval(() => {
-      //   document.title = 'TextUtils is Amazing Mode';
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = 'Install TextUtils Now';
-      // }, 1500);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-      document.title = "TextUtils - Light Mode";
     }
   };
   return (
     <>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-      {/* <Navbar/> */}
       <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Navbar
+          title="TextUtils"
+          mode={mode}
+          toggleMode={toggleMode}
+          key={new Date()}
+        />
         <Alert alert={alert} />
         <div className="container my-3">
           <Switch>
             {/* /users --> Component 1
         /users/home --> Component 2 */}
             <Route exact path="/about">
-              <About />
+              <About mode={mode} />
             </Route>
             <Route exact path="/">
               <Textform
                 showAlert={showAlert}
-                heading="Enter the text to analyze below"
+                heading="Try TextUtils - word counter, character counter, remove extra spaces"
                 mode={mode}
               />
             </Route>
